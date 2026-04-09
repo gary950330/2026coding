ce6efe8b28d6fe92d96bc46619b2d0ac4da310db
gary950330 <gary033095@gmail.com>
2026-04-09 16:30:50 +0800

    Delete week07 directory

--- week07/week07-3.py
+++
@@ -1,9 +0,0 @@
-# week07-3.py 學習計畫 stack 第一題
-# LeetCode 2390. Removing Stars From a String
-class Solution:
-    def removeStars(self, s: str) -> str:
-        ans = []
-        for c in s:
-            if c=='*': ans.pop()
-            else: ans.append(c)
-        return ''.join(ans)
